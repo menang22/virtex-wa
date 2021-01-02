241f392aada13d779b3facd2d96c1fc4accbe859
The Hackers 22 <76223089+menang22@users.noreply.github.com>
2021-01-02 22:51:34 +0700

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+import os
+import sys
+import time
+from time import sleep as out
+import subprocess
+def bersih():
+    os.system("clear")
+def kembali():
+    ye = input("coba lagi? (y/t): ")
+    if ye == "y":
+       subprocess.call("python virus.py",shell=True)
+    elif ye == "t":
+         print ("Dadah woiii")
+         time.sleep(1)
+         os.system("exit")
+    else:
+         print ("Tidak ada Input Bujank!!!")
+         time.sleep(1)
+         kembali()
+def main(s):
+  for c in s + '\n':
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(1./12)
+def jamet():
+    bersih()
+    f = input("Apa ente Udah subrek Channel Admin? (y/t): ")
+    if f == "y":
+       print ("Thanks you")
+       time.sleep(1)
+    elif f == "t":
+         print ("Subrek dulu Gan")
+         main("Loading")
+         main("...................[Success]")
+         time.sleep(1)
+         subprocess.call("am start https://www.youtube.com/channel/UCS4EpzW-9kWTIt3Dmzb4oQQ",shell=True)
+         time.sleep(3)
+         print ("ThanksYou")
+         time.sleep(1)
+    else:
+         print ("Tidak ada Nanaon Gan:v")
+         time.sleep(1)
+         print ("Balik deui")
+         time.sleep(1)
+         subprocess.call("python virus.py",shell=True)
+jamet()
+bersih()
+subprocess.call("figlet Virus Wa |lolcat",shell=True)
+banner = """
+\x1b[1;97m====================================================
+Coded\x1b[1;91m:\x1b[1;96mGabriel.S\x1b[1;97m
+Youtube\x1b[1;91m:\x1b[1;96mWinnerChannel\x1b[1;97m
+Github\x1b[1;91m:\x1b[1;92mhttps://github.com/menang22
+\x1b[1;97m====================================================
+"""
+print (banner)
+print ("\x1b[1;97m[\x1b[1;96m01\x1b[1;97m].\x1b[1;92mVirus Arab")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m02\x1b[1;97m].\x1b[1;92mVirus Banamex")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m03\x1b[1;97m].\x1b[1;92mVirpen")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m04\x1b[1;97m].\x1b[1;92mVirus Wolf")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m05\x1b[1;97m].\x1b[1;92mVirus Idn")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m06\x1b[1;97m].\x1b[1;92mVirus Attacker Alone")
+time.sleep(0.2)
+print ("\x1b[1;97m[\x1b[1;96m07\x1b[1;97m].\x1b[1;92mDownload All Virtex")
+time.sleep(0.2)
+
+print ("\x1b[1;97m[\x1b[1;96m00\x1b[1;97m].\x1b[1;92mExit")
+time.sleep(0.2)
+print ("\033[1;97m")
+thex = input(">> ")
+if thex == "01" or thex == "1":
+    print ("Loading...")
+    subprocess.call("cat virab.txt",shell=True)
+elif thex == "02" or thex == "2":
+      print ("Loading...")
+      time.sleep(1)
+      subprocess.call("cat banamex.txt",shell=True)
+      kembali()
+elif thex == "03" or thex == "3":
+      print ("Loading...")
+      time.sleep(1)
+      subprocess.call("cat virpen.txt",shell=True)
+      kembali()
+elif thex == "04" or thex == "4":
+      print ("Loading")
+      main("..................")
+      time.sleep(1)
+      subprocess.call("cat wolf.txt",shell=True)
+      kembali()
+elif thex == "05" or thex == "5":
+      print ("Loading...")
+      main("..................")
+      time.sleep(1)
+      subprocess.call("cat idn.txt",shell=True)
+      kembali()
+elif thex == "06" or thex == "6":
+      print ("Loading")
+      main("...................")
+      time.sleep(1)
+      subprocess.call("cat alone.txt",shell=True)
+      kembali()
+elif thex == "07" or thex == "7":
+      print ("Loading")
+      main("...................")
+      time.sleep(1)
+      subprocess.call("unzip virus.zip",shell=True)
+      time.sleep(2)
+      print ("[\x1b[1;92mSuccess\x1b[1;97m] Downloading Compleate...")
+      time.sleep(1)
+      print ("file tersimpan di folder ini")
+      time.sleep(1)
+      print ("ketikan cp -f -r namafileataufolder /sdcard untuk memindahkan virtexnya")
+      time.sleep(2)
+      kembali()
+elif thex == "00" or thex == "0":
+      print ("Close")
+      time.sleep(0.3)
+      os.system("exit")
+else:
+      print ("Input Eweh Gan")
+      time.sleep(1)
+      budek = input("Arek balik deui apa arek kaluar? (y/t): ")
+      if budek == "y":
+         subprocess.call("python virus.py",shell=True)
+      elif budek == "t":
+           subprocess.call("exit",shell=True)
+      else:
+           print ("Salah Ketik Gan")
+           subprocess.call("python virus.py",shell=True)
+      
